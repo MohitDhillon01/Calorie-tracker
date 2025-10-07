37acab2837b36f4366aa83110dff1f66b2c546f3
MohitDhillon01 <mohitdhillon1811@gmail.com>
2025-10-07 22:33:34 +0530

    Update #Mohit Dhillon, 6Th OCT 2025.py

--- #Mohit Dhillon, 6Th OCT 2025.py
+++ #Mohit Dhillon, 6Th OCT 2025.py
@@ -1,29 +1,29 @@
 #Mohit Dhillon, 6Th OCT 2025 
 #Daily Calorie Tracker
-print('Hello Users')
-print('This is a Daily calorie tracker you can track your daily calorie intake with it.')
+print('Welcome')
+print('This is a Daily calorie tracker you can track your daily calorie input with it.')
 #task2
 Meal=[]
 Calories=[]
 num=int(input('enter the no. of meals you had today:'))
 for i in range(1,num+1):
-    Mname=input('enter meal name:')
+    Mealname=input('enter meal name:')
     Howcal=int(input('enter the number of calories in this meal:'))
-    Meal.append(Mname)
+    Meal.append(Mealname)
     Calories.append(Howcal)
 print(Meal)
 print(Calories)
 #task3
-dailyin=int(input('enter your daily calorie intake limit:'))
-totcal=sum(Calories)
-print('your total calorie intake today is:',totcal)
+dailyinput=int(input('enter your daily calorie input limit:'))
+totalcalories=sum(Calories)
+print('your total calorie input today is:',totalcalories)
 #task4
-if totcal>dailyin:
-    print('WARNING!!! you have exceeded your daily calorie intake limit')
+if totalcalories>dailyinput:
+    print('WARNING!!! you have exceeded your daily calorie input limit')
 else:
-    print('GOOD you are within your daily calorie intake limit')
-avgcal=totcal/num
-print('your average calorie intake per meal is:',avgcal)
+    print('GOOD you are within your daily calorie input limit')
+avgcalories=totalcalories/num
+print('your average calorie input per meal is:',avgcalories)
 
 #food calorie review table 
 print("MEAL\t\tCALORIES\tREVIEW")
